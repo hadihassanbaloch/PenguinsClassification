57a65a3989cf8366c22ff017cb0963fa350ebc07
Hadi Hassan <106299673+hadihassanbaloch@users.noreply.github.com>
2024-08-25 15:33:29 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import  pandas as pd
 
 st.title('🤖 Machine Learning App')
 
-st.write('This app is to create a machine learning model')
+st.info('This app is to create a machine learning model')
+
+with st.expander("View the Data"):
+  st.write("**Penguin Dataset**")
+  df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+  df
+  
