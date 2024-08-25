93ae469f354010ed879901d0e711bc37f741e706
Hadi Hassan <106299673+hadihassanbaloch@users.noreply.github.com>
2024-08-25 16:19:53 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,11 +20,18 @@ with st.expander("Data Visualization"):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g',  color='species')
   
 #Data preparation
-#"bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g",
+#,"flipper_length_mm","body_mass_g",
 with st.sidebar:
   st.header("**Input Features**")
   island = st.selectbox("Islands", ("Torgersen", "Biscoe", "Dream"))
   st.write("You have selected:", island)
   gender = st.selectbox("Gender", ("Male", "Female"))
   st.write("You have selected:", gender)
-  bill_length_mm = st.slider("Select a range from:", 32.1,59.6, 43.1)
+  bill_length_mm = st.slider("Bill length (mm):", 32.1,59.6, 43.1)
+  st.write("You have selected:", bill_length_mm )
+  bill_depth_mm = st.slider(" Bill depth (mm): ",13.1, 21.5,17.2)
+  st.write("You have selected:",  bill depth_mm )
+  flipper_length_mm = st.slider("Flipper length (mm): ",172.0, 231.0, 201.0)
+  st.write("You have selected:",  flipper_length_mm)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+  st.write("You have selected:",  body_mass_g)
