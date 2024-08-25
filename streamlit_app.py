2cb857234fcd8c8c910306fecac6cb79dba833af
Hadi Hassan <106299673+hadihassanbaloch@users.noreply.github.com>
2024-08-25 16:44:20 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,9 @@ data = {
 }
 input_data = pd.DataFrame(data, index=[0])
 input_penguins = pd.concat([input_data, X], axis=0)
+
+with st.expander('Input features'):
+  st.write('**Input penguin**')
+  input_df
+  st.write('**Combined penguins data**')
+  input_penguins
