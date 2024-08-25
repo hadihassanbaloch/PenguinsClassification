0ae8ceea1f845ba9bb1607c2a5e533f06afad5a2
Hadi Hassan <106299673+hadihassanbaloch@users.noreply.github.com>
2024-08-25 16:50:56 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,11 @@ with st.expander("View the Data"):
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
   df
   st.write("**Features**")
-  X = df.drop("species",axis=1)
-  X
+  X_raw = df.drop("species",axis=1)
+  X_raw
   st.write("**Target**")
-  y = df.species
-  y
+  y_raw = df.species
+  y_raw
 with st.expander("Data Visualization"):
   st.write("** ScatterPlot to compare difference in body length with body mass based on species**")
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g',  color='species')
@@ -45,10 +45,30 @@ data = {
   "sex": gender
 }
 input_data = pd.DataFrame(data, index=[0])
-input_penguins = pd.concat([input_data, X], axis=0)
+input_penguins = pd.concat([input_data, X_raw], axis=0)
 
 with st.expander('Input features'):
   st.write('**Input penguin**')
   input_data
   st.write('**Combined penguins data**')
   input_penguins
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+
+X = df_penguins[1:]
+input_row = df_penguins[:1]
+
+# Encode y
+target_mapper = {'Adelie': 0,
+                 'Chinstrap': 1,
+                 'Gentoo': 2}
+def target_encode(val):
+  return target_mapper[val]
+
+y = y_raw.apply(target_encode)
+
+with st.expander('Data preparation'):
+  st.write('**Encoded X (input penguin)**')
+  input_row
+  st.write('**Encoded y**')
+  y
