a2f4d8d7dbc386fa5d1f1da2e4ca5f99edd35ee7
Hadi Hassan <106299673+hadihassanbaloch@users.noreply.github.com>
2024-08-25 16:20:28 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ with st.sidebar:
   bill_length_mm = st.slider("Bill length (mm):", 32.1,59.6, 43.1)
   st.write("You have selected:", bill_length_mm )
   bill_depth_mm = st.slider(" Bill depth (mm): ",13.1, 21.5,17.2)
-  st.write("You have selected:",  bill depth_mm )
+  st.write("You have selected:",  bill_depth_mm)
   flipper_length_mm = st.slider("Flipper length (mm): ",172.0, 231.0, 201.0)
   st.write("You have selected:",  flipper_length_mm)
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
